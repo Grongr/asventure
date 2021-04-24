afe8c250b4f86ed3bdb175d30eafb79f0ebcfada
jesuslovesyoutoday <jesuslovesyoutoday@yandex.ru>
2021-04-24 13:30:42 +0300

    beta mechanics of ship moving

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main() {
 	SpaceShipBuilder ssb;
 	ssb.set_fuel_cost(1);
 	ssb.set_is_engine_active(false);
-	ssb.set_AVec(Vector(0,0));
+	ssb.set_AVec(Vector(0, 0));
 	ssb.set_R(Vector(0,0));
 	ssb.set_V(Vector(0,0));
 	ssb.set_mass(0);
@@ -31,12 +31,16 @@ int main() {
 	
 	sf::FloatRect rect;
 	rect = sf::FloatRect(160, 160, 32, 32);
+	int i = 10;
+
+	ship->toggle_engine();
 
 	while(window.isOpen())
 	{
 		sf::Clock clock;
 		double time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
+		time = time/1000;
 
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -45,18 +49,47 @@ int main() {
 				window.close();
 		}
 
+		/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		{
+			ship->toggle_engine();
+		}*/
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			ship->set_velocity(Vector(-0.1, 0) + (ship->get_velocity()));
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			ship->set_velocity(Vector(0.1, 0) + (ship->get_velocity()));
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			ship->set_velocity(Vector(0, -0.1) + (ship->get_velocity()));
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			ship->set_velocity(Vector(0, 0.1) + (ship->get_velocity()));
+		}
+
+		//std::cout << (ship->get_accel().x_pos()) << (ship->get_accel().y_pos()) << std::endl;
+		//time = time*100;
 		ship->move_ship(time);
 		Vector vec;
 		vec = ship->get_position();
 		rect.left = vec.x_pos();
 		rect.top = vec.y_pos();
-		rect.left = 600;
-		rect.top = 600;
 		
 		shape.setPosition(rect.left, rect.top);
+		//std::cout << "x = " << rect.left << " " << "y = " << rect.top;
+		//std::cout << "time: " << time;
 		window.clear();
 		window.draw(shape);
 		window.display();
+
+		i += 10;
 	}
 
 	//unit_test();
