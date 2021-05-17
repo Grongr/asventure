fedaa01a2ebd09af13b2bd4d2ef7920410a35c8e
Egor Osipov <e.a.osipov@mail.ru>
2021-05-18 00:19:54 +0300

    Debug

--- geometricvector.cpp
+++ geometricvector.cpp
@@ -40,8 +40,4 @@ bool Vector::operator==(const Vector &r) const {
 
 void Vector::print_vector() const {
     std::cout << this->x << " " << this->y << std::endl;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
